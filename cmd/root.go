4519b5e200602ec306a8d58d73c3a7431c282b91
igorplebanczyk <plebanczyk.igor@gmail.com>
2024-09-29 12:52:37 +0200

    Setup spf13/cobra

---
+++ cmd/root.go
@@ -0,0 +1,13 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var rootCmd = &cobra.Command{
+	Use:   "cachprax",
+	Short: "A simple caching proxy",
+	Long:  "A simple proxy server that forwards requests to a target server and caches the responses.",
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
--- main.go
+++ main.go
@@ -3,18 +3,11 @@ package main
 import (
 	"cachprax/cmd"
 	"fmt"
-	"os"
 )
 
 func main() {
-	cliApp := cmd.NewApp()
-	err := cliApp.Run(os.Args)
+	err := cmd.Execute()
 	if err != nil {
-		fmt.Printf("error running cli: %v", err)
-	}
-
-	_, err = fmt.Fprintf(os.Stdout, "\n") // Add a line break for cleaner output
-	if err != nil {
-		return
+		fmt.Println(err)
 	}
 }
