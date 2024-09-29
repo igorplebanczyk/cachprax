095524bf5fc9d19f56572b36b4ede0046404e772
igorplebanczyk <plebanczyk.igor@gmail.com>
2024-09-29 13:23:46 +0200

    Switch runserver to Cobra

--- cmd/runserver.go
+++ cmd/runserver.go
@@ -4,29 +4,65 @@ import (
 	"cachprax/internal/cache"
 	"cachprax/internal/server"
 	"fmt"
-	"github.com/urfave/cli/v2"
+	"github.com/spf13/cobra"
 	"net/url"
 	"time"
 )
 
-func runserverCommand(c *cli.Context) error {
-	if !c.Bool("override") {
-		return fmt.Errorf("this command should not be ran manually; use the start command instead")
-	}
-
-	port := c.Int("port")
-	originURL, err := url.Parse(c.String("origin"))
+func runserverCommand(cmd *cobra.Command, _ []string) error {
+	override, err := cmd.Flags().GetBool("override")
 	if err != nil {
 		return err
 	}
-	cacheExpire := time.Duration(c.Int("cache-expire")) * time.Minute
-	cachePurge := time.Duration(c.Int("cache-purge")) * time.Minute
+	if !override {
+		return fmt.Errorf("this command should not be run manually; use the start command instead")
+	}
+
+	port, err := cmd.Flags().GetInt("port")
+	if err != nil {
+		return err
+	}
+
+	origin, err := cmd.Flags().GetString("origin")
+	if err != nil {
+		return err
+	}
+	originURL, err := url.Parse(origin)
+	if err != nil {
+		return err
+	}
+
+	cacheExpire, err := cmd.Flags().GetInt("cache-expire")
+	if err != nil {
+		return err
+	}
+	cachePurge, err := cmd.Flags().GetInt("cache-purge")
+	if err != nil {
+		return err
+	}
 
 	cfg := &server.Config{
 		Port:   port,
 		Origin: originURL,
-		Cache:  cache.NewCache(cacheExpire, cachePurge),
+		Cache:  cache.NewCache(time.Duration(cacheExpire)*time.Minute, time.Duration(cachePurge)*time.Minute),
 	}
 
 	return cfg.StartServer()
+}
+
+var runserverCmd = &cobra.Command{
+	Use:   "runserver",
+	Short: "Start the proxy server manually (internal use)",
+	Long:  "This command starts the proxy server manually, but it should be run only via the start command.",
+	RunE:  runserverCommand,
+}
+
+func init() {
+	rootCmd.AddCommand(runserverCmd)
+
+	runserverCmd.Flags().Bool("override", false, "Override manual check (for internal use only)")
+	runserverCmd.Flags().IntP("port", "p", 8080, "Port to run the server on")
+	runserverCmd.Flags().StringP("origin", "o", "", "Origin server URL")
+	runserverCmd.Flags().Int("cache-expire", 10, "Cache expiration duration in minutes")
+	runserverCmd.Flags().Int("cache-purge", 30, "Cache purge interval in minutes")
 }
