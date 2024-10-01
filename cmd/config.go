259390a7634c6bd7b6cf01a344bde2c05eabd2df
igorplebanczyk <plebanczyk.igor@gmail.com>
2024-10-01 18:43:04 +0200

    Add a config command for modifying the config file via the CLI

---
+++ cmd/config.go
@@ -0,0 +1,74 @@
+package cmd
+
+import (
+	"cachprax/internal/config"
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"os"
+	"path/filepath"
+)
+
+func configCommand(cmd *cobra.Command, args []string) error {
+	reset, err := cmd.Flags().GetBool("reset")
+	if err != nil {
+		return fmt.Errorf("could not get reset flag: %v", err)
+	}
+	set, err := cmd.Flags().GetBool("set")
+	if err != nil {
+		return fmt.Errorf("could not get set flag: %v", err)
+	}
+
+	if reset {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return fmt.Errorf("could not get user home directory: %v", err)
+		}
+
+		configFile := config.FileName + "." + config.FileType
+		configPath := filepath.Join(home, configFile)
+
+		err = os.WriteFile(configPath, config.DefaultValues, 0644)
+		if err != nil {
+			return fmt.Errorf("could not write default config file: %v", err)
+		}
+
+		fmt.Println("Configuration file reset to default values.")
+		return nil
+	}
+
+	if set {
+		if len(args) != 2 {
+			return fmt.Errorf("set flag requires a key and a value")
+		}
+
+		key := args[0]
+		value := args[1]
+
+		viper.Set(key, value)
+
+		err = viper.WriteConfig()
+		if err != nil {
+			return fmt.Errorf("could not write config file: %v", err)
+		}
+
+		fmt.Printf("Config value %s set to %s\n", key, value)
+		return nil
+	}
+
+	return fmt.Errorf("either --reset or --set flag must be provided")
+}
+
+var configCmd = &cobra.Command{
+	Use:   "config",
+	Short: "Manage configuration values",
+	Long:  "Manage configuration values in the cachprax.yml file.",
+	RunE:  configCommand,
+}
+
+func init() {
+	rootCmd.AddCommand(configCmd)
+
+	configCmd.Flags().BoolP("reset", "r", false, "Reset the configuration file to default values")
+	configCmd.Flags().BoolP("set", "s", false, "Set a configuration value")
+}
--- internal/config/config.go
+++ internal/config/config.go
@@ -7,9 +7,9 @@ import (
 	"path/filepath"
 )
 
-var configFileName = "cachprax"
-var configFileType = "yaml"
-var defaultConfig = []byte(`proxy_port: 3000
+var FileName = "cachprax"
+var FileType = "yaml"
+var DefaultValues = []byte(`proxy_port: 3000
 cache_port: 3001
 cache_expire: 10
 cache_purge: 30
@@ -22,19 +22,19 @@ func Init() error {
 		return fmt.Errorf("could not get user home directory: %v", err)
 	}
 
-	configFile := configFileName + "." + configFileType
+	configFile := FileName + "." + FileType
 	configPath := filepath.Join(home, configFile)
 
 	// Create default config file if it does not exist
 	if _, err := os.Stat(configPath); os.IsNotExist(err) {
-		err = os.WriteFile(configPath, defaultConfig, 0644)
+		err = os.WriteFile(configPath, DefaultValues, 0644)
 		if err != nil {
 			return fmt.Errorf("could not write default config state: %v", err)
 		}
 	}
 
-	viper.SetConfigName(configFileName)
-	viper.SetConfigType(configFileType)
+	viper.SetConfigName(FileName)
+	viper.SetConfigType(FileType)
 	viper.AddConfigPath(home)
 
 	if err := viper.ReadInConfig(); err != nil {
